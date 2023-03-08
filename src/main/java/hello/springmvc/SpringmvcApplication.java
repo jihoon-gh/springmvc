cb96577976c05b59e2f3ac18743b30eeb986b063
jihoon-gh <jihoon806@gmail.com>
2023-03-08 18:12:21 +0900

    [FEAT] 요청 관련 정리 (test, json)

--- src/main/java/hello/springmvc/SpringmvcApplication.java
+++ src/main/java/hello/springmvc/SpringmvcApplication.java
@@ -3,11 +3,15 @@ package hello.springmvc;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+
 @SpringBootApplication
 public class SpringmvcApplication {
 	//sl4j = 인터페이스, logback = sl4j 구현체
 	public static void main(String[] args) {
 		SpringApplication.run(SpringmvcApplication.class, args);
-	}
 
+
+
+	}
 }
